b0cfc419d86cdb70267d237c083b24574a02d620
Mazino <fredozv01@gmail.com>
2025-01-30 00:22:02 +0100

    fix ping

--- api/ping.js
+++ api/ping.js
@@ -1,9 +1,10 @@
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-      console.log('Ping reçu...');
-      res.status(200).json({ message: 'Bot actif' });
-    } else {
-      res.status(405).json({ message: 'Méthode non autorisée' });
-    }
+export default function handler(req, res) {
+  // Vérifie la méthode de la requête
+  if (req.method === 'GET' || req.method === 'HEAD') {
+    // Répond avec le message "Bot is online"
+    res.status(200).json({ message: 'Bot is online' });
+  } else {
+    // Méthode non autorisée
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
-  
+}
